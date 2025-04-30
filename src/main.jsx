9c4c4da2f806f590bed3071edf0b8d0758670b35
see-a-theater <seeatheater@gmail.com>
2025-04-30 09:45:07 +0000

    [REMOVE] 불필요한 css 파일 삭제

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import App from './App.jsx';
+import theme from './style/Theme.jsx';
+import GlobalStyle from './style/GlobalStyle.jsx';
+import { ThemeProvider } from 'styled-components';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
-		<App />
+		<ThemeProvider theme={theme}>
+			<GlobalStyle />
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</ThemeProvider>
 	</StrictMode>,
 );
