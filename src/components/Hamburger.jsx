b37737028ed3b3c71a1fe48bb5d8aef3e6105228
jiwon234 <145983750+jiwon234@users.noreply.github.com>
2025-06-16 21:35:48 +0900

    [FEAT] 풀스크린메뉴 햄버거바에 연결

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -4,18 +4,37 @@ import HamburgerIcon from '@/assets/icons/hamburger.svg?react';
 import Alrert from '@/assets/icons/alrert.svg?react';
 import Search from '@/assets/icons/search.svg?react';
 import Logo from '@/assets/icons/logo.svg?react';
-function Hamburger({ hasLogo }) {
-	return (
-		<Container>
-			<ButtonArea>
-				{hasLogo ? <Logo /> : <HamburgerIcon alt="햄버거바" height={15} />}
+import FullScreenMenu from './FullScreenMenu';
+import { useState } from 'react';
 
-				<Right>
-					<Alrert alt="알림" />
-					<Search alt="검색" />
-				</Right>
-			</ButtonArea>
-		</Container>
+/* 햄버거에 FullScreenMenu 연결 시 레이아웃 문제가 생겨 
+해당 컴포넌트의 아이콘 간격 조정했습니다. */
+
+function Hamburger({ hasLogo }) {
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
+	return (
+		<>
+			{!isMenuOpen && (
+				<Container>
+					<ButtonArea>
+						{hasLogo ? (
+							<Logo />
+						) : (
+							<HamburgerIcon
+								alt="햄버거바"
+								height={15}
+								onClick={() => setIsMenuOpen(true)}
+							/>
+						)}
+						<Right>
+							<Alrert alt="알림" />
+							<Search alt="검색" />
+						</Right>
+					</ButtonArea>
+				</Container>
+			)}
+			{isMenuOpen && <FullScreenMenu onClose={() => setIsMenuOpen(false)} />}
+		</>
 	);
 }
 
@@ -26,14 +45,14 @@ const Container = styled.div`
 	background: ${({ theme }) => theme.colors.ivoryBg};
 
 	position: relative;
-	padding: 0 20px;
+	padding: 0px 20px;
 `;
 
 const ButtonArea = styled.div`
 	position: absolute;
 	bottom: 20px;
-	left: 0;
-	right: 0;
+	left: 20px; /*0 -> 20px로 변경*/
+	right: 20px; /* 0 -> 20px로 변경*/
 
 	display: flex;
 	justify-content: space-between;
